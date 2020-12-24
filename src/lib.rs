5687e5c19ff3d68b5f6ecb8077bbe4c195438ea5
Ken <kenakioreed@gmail.com>
2020-12-23 19:23:16 -0500

    better docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! A crate that provides the user with two fast "vec-like" vecs that are backed by
 //! a single vec. The caveat is that the operations like push and truncate
 //! may rearrange the order of the other vec in an unspecified way.
-//!
-
+//! Also provides a `retain_mut_unordered` function to both the regular `Vec` as well as
+//! the two "vec-like" vecs provided by this crate.
 
 
 
@@ -217,7 +217,8 @@ impl<T> RetainMutUnordered<T> for Vec<T>{
     }
 }
 
-
+/// Retain only elements that satisfy the predicate.
+/// May rearrange elements in the process.
 pub trait RetainMutUnordered<T> {
     fn truncate(&mut self,val:usize);
     fn as_slice_mut(&mut self)->&mut [T];
